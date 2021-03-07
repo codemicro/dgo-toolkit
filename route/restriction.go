62a1a47eaae3e598c6112f87bdb8fdf85250da02
Tom <tom@tdpain.net>
2021-03-07 20:35:27 +0000

    Add prefab CommandRestriction generators

--- route/restriction.go
+++ route/restriction.go
@@ -1,9 +1,35 @@
 package route
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"github.com/bwmarrin/discordgo"
+	"strings"
+)
 
-// CommandRestriction is a function that returns true if a command can be run based on the current state (eg. user, roles,
-// channel, etc)
+// CommandRestriction is a function that returns true if a command can be run based on the current state (eg. user,
+// roles, channel, etc)
 type CommandRestriction func(session *discordgo.Session, message *discordgo.MessageCreate) (bool, error)
 
-// TODO: premade restriction templates, eg restriction by role, by channel, etc...
+func isStringInSlice(needle string, haystack []string) (found bool) {
+	for _, v := range haystack {
+		if strings.EqualFold(needle, v) {
+			found = true
+			break
+		}
+	}
+	return
+}
+
+// RestrictionByRole creates a CommandRestriction that requires the commanding guild member to have a given role ID
+func RestrictionByRole(roleId string) CommandRestriction {
+	return func(_ *discordgo.Session, message *discordgo.MessageCreate) (bool, error) {
+		return isStringInSlice(roleId, message.Member.Roles), nil
+	}
+}
+
+// RestrictionByChannel creates a CommandRestriction that requires the command to have been sent in a channel with a
+// given ID
+func RestrictionByChannel(channelIds ...string) CommandRestriction {
+	return func(_ *discordgo.Session, message *discordgo.MessageCreate) (bool, error) {
+		return isStringInSlice(message.ChannelID, channelIds), nil
+	}
+}
